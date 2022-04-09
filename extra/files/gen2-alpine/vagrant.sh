d6b92f14ad5fdd58c4ca4319562f7d77fa2e1ad9
Windom <drw_08@hotmail.com>
2022-04-09 17:34:08 +0800

    pupdate vagrant key

--- extra/files/gen2-alpine/vagrant.sh
+++ extra/files/gen2-alpine/vagrant.sh
@@ -18,7 +18,7 @@ echo "vagrant:vagrant" | chpasswd
 
 mkdir -pm 700 /home/vagrant/.ssh
 
-curl -sSo /home/vagrant/.ssh/authorized_keys 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub'
+curl -sSo /home/vagrant/.ssh/authorized_keys 'https://raw.githubusercontent.com/hashicorp/vagrant/main/keys/vagrant.pub'
 
 chown -R vagrant:vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
